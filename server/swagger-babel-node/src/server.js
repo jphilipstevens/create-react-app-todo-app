83b9575f872dffd0ae5af040df62ea70b03b93a0
Jonathan Stevens <jonathan.philip.stevens@gmail.com>
2017-03-09 13:23:37 -0500

    - fixed server startup script - added checks for file access to client build and exit condition

--- server/swagger-babel-node/src/server.js
+++ server/swagger-babel-node/src/server.js
@@ -5,6 +5,18 @@ import path from "path";
 import morgan from "morgan";
 import SwaggerExpress from "swagger-express-mw";
 import ArgParser from "args-parser";
+import fs from "fs";
+
+const args = ArgParser(process.argv);
+
+fs.access(path.resolve(args.clientPath), fs.constants.R_OK, (error) => {
+    if (error) {
+        console.error("Error while finding client code");
+        console.error(error);
+        console.error(`Path specified was ${path.resolve(args.clientPath)}`);
+        process.exit(1);
+    }
+});
 
 require("./module-registration");
 
@@ -16,7 +28,7 @@ const config = {
     appRoot: appRoot,
     configDir: path.resolve(path.join(appRoot, "config")),
     swaggerFile: path.resolve(path.join(appRoot, "config", "swagger.json")),
-    clientPath: ArgParser(process.argv).clientPath
+    clientPath: args.clientPath
 };
 
 SwaggerExpress.create(config, (err, swaggerExpress) => {
@@ -40,6 +52,7 @@ SwaggerExpress.create(config, (err, swaggerExpress) => {
     const port = process.env.PORT || 8080;
     app.listen(port);
 
+    console.log(`Using client path ${path.resolve(config.clientPath)}`);
     console.log(`running the app on port ${port}`);
 });
 
